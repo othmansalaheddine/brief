09a00cf6718b2d897deb0d4f2a3aad1ff695ff49
imane-bouabidi <imaneyoucode@gmail.com>
2023-12-05 14:41:56 +0100

    modifier index

--- index.php
+++ index.php
@@ -21,6 +21,12 @@
         <!-- Header Navbar -->
     <?php
       include('header.php');
+
+      $nom = "";
+      $desc = "";
+      $img = "";
+      $price = "";
+      $quantity = "";
       ?>
         
  
@@ -132,39 +138,35 @@
                     $totalPossiblePages = ceil($totalMatchingProducts / $pageSize);
                     if ($result->num_rows > 0) {
                         while (($row = $result->fetch_assoc()) ) {
-                            echo '
+                            ?>
                             <article class="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
                                 <a >
                                 <div class="relative flex items-end overflow-hidden rounded-xl">
-                                    <img src="assets/image/' . $row['image'] . '" alt="Hotel Photo" />
+                                    <img src="assets/image/<?php echo  $row['image']; ?>" alt="Hotel Photo" />
                                     <div class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                     </svg>
- 
-                                    <button class="text-sm">Add to cart</button>
                                     </div>
                                 </div>
  
                                 <div class="mt-1 p-2">
-                                    <h2 class="text-slate-700">' . $row['name'] . '</h2>
-                                    <p class="mt-1 text-sm text-slate-400">' . $row['city'] . ', ' . $row['country'] . '</p>
- 
+                                    <h2 class="text-slate-700"><?php echo $row['name']; ?></h2> 
                                     <div class="mt-3 flex items-end justify-between">
-                                        <p class="text-lg font-bold text-blue-500">$' . $row['new_price'] . '</p>
- 
+                                        <p class="text-lg font-bold text-blue-500">$<?php echo  $row['new_price']; ?></p>
+   
                                     <div class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                                         </svg>
  
-                                        <button class="text-sm">Add to cart</button>
+                                        <a href="#?id=<?php echo $row['id']; ?>"><button class="text-sm">Add to cart</button></a>
                                     </div>
                                     </div>
                                 </div>
                                 </a>
                             </article>
-                        ';
+                        <?php
                         }
                     }
                     $Tprice = 9999;
