59a34d0c921b970d93c453de113b7211c8e9b363
FOUDIAHMED <foudiahmed82@gmail.com>
2023-12-05 14:23:09 +0100

    ahmed

--- back/database/brief6.session.sql
+++ back/database/brief6.session.sql
@@ -28,7 +28,9 @@ CREATE TABLE product (
    stock int,
    country varchar(255),
    city varchar(255),
+   nbachat int not null,
    category int,
+   
    FOREIGN KEY (category) REFERENCES category(id) on delete cascade
 );
 -- @block
@@ -37,28 +39,34 @@ INSERT INTO category (name, description, image) VALUES
 ("electrique","adadadadadada", "New York"),
 ("switch","adadadadadada", "New York");
 -- @block
-INSERT INTO product (name, old_price, new_price, category, image, stock, country, city) VALUES
-("LED", 0.5, 0.25, 1, "LED.jpg", 12, "USA", "New York"),
-("Resistor", 0.1, 0.05, 3, "Resistor.jpg", 5, "Canada", "Toronto"),
+INSERT INTO product (name, old_price, new_price, category, image, stock, country, city,nbachat) VALUES
+("LED", 0.5, 0.25, 1, "LED.jpg", 12, "USA", "New York",10),
+("Resistor", 0.1, 0.05, 3, "Resistor.jpg", 5, "Canada", "Toronto",20),
 ("Arduino", 10.0, 5.0, 2, "arduino.jpg", 4, "Germany", "Berlin"),
-("Raspberry Pi", 35.0, 17.5, 2, "Raspberry_Pi.jpg", 1, "France", "Paris"),
-("Capacitor", 0.2, 0.1, 1, "Capacitor.jpg", 0, "Australia", "Sydney"),
-("Transistor", 0.3, 0.15, 1, "Transistor.jpg", 3, "Japan", "Tokyo"),
-("Diode", 0.2, 0.1, 1, "Diode.jpg", 7, "Brazil", "Rio de Janeiro"),
-("Potentiometer", 1.0, 0.5, 3, "Potentiometer.jpg", 19, "India", "Mumbai"),
-("Relay", 2.0, 1.0, 2, "Relay.jpg", 102, "China", "Shanghai"),
-("IC", 1.5, 0.75, 1, "IC.jpg", 45, "Spain", "Madrid"),
-("Breadboard", 5.0, 2.5, 2, "Breadboard.jpg", 5, "United Kingdom", "London"),
-("Motor", 8.0, 4.0, 2, "Motor.jpg", 60, "South Korea", "Seoul"),
-("Sensor", 3.0, 1.5, 1, "Sensor.jpg", 12, "Mexico", "Mexico City"),
-("Switch", 0.5, 0.25, 1, "Switch.jpg", 45, "Italy", "Rome"),
-("Battery", 2.0, 1.0, 2, "Battery.jpg", 300, "Russia", "Moscow"),
-("Power Supply", 10.0, 5.0, 2, "Power_Supply.jpg", 4, "Sweden", "Stockholm"),
-("Wire", 0.1, 0.05, 1, "Wire.jpg", 1000, "Netherlands", "Amsterdam"),
-("Connector", 0.3, 0.15, 3, "Connector.jpg", 30, "Switzerland", "Zurich"),
-("PCB", 5.0, 2.5, 2, "PCB.jpg", 4, "Belgium", "Brussels"),
-("Oscillator", 1.5, 0.75, 1, "Oscillator.jpg", 20, "Argentina", "Buenos Aires");
+("Raspberry Pi", 35.0, 17.5, 2, "Raspberry_Pi.jpg", 1, "France", "Paris",30),
+("Capacitor", 0.2, 0.1, 1, "Capacitor.jpg", 0, "Australia", "Sydney",40),
+("Transistor", 0.3, 0.15, 1, "Transistor.jpg", 3, "Japan", "Tokyo",41),
+("Diode", 0.2, 0.1, 1, "Diode.jpg", 7, "Brazil", "Rio de Janeiro",35),
+("Potentiometer", 1.0, 0.5, 3, "Potentiometer.jpg", 19, "India", "Mumbai",46),
+("Relay", 2.0, 1.0, 2, "Relay.jpg", 102, "China", "Shanghai",87),
+("IC", 1.5, 0.75, 1, "IC.jpg", 45, "Spain", "Madrid",34),
+("Breadboard", 5.0, 2.5, 2, "Breadboard.jpg", 5, "United Kingdom", "London",45),
+("Motor", 8.0, 4.0, 2, "Motor.jpg", 60, "South Korea", "Seoul",67),
+("Sensor", 3.0, 1.5, 1, "Sensor.jpg", 12, "Mexico", "Mexico City",21),
+("Switch", 0.5, 0.25, 1, "Switch.jpg", 45, "Italy", "Rome",34),
+("Battery", 2.0, 1.0, 2, "Battery.jpg", 300, "Russia", "Moscow",54),
+("Power Supply", 10.0, 5.0, 2, "Power_Supply.jpg", 4, "Sweden", "Stockholm",56),
+("Wire", 0.1, 0.05, 1, "Wire.jpg", 1000, "Netherlands", "Amsterdam",56),
+("Connector", 0.3, 0.15, 3, "Connector.jpg", 30, "Switzerland", "Zurich",54),
+("PCB", 5.0, 2.5, 2, "PCB.jpg", 4, "Belgium", "Brussels",6),
+("Oscillator", 1.5, 0.75, 1, "Oscillator.jpg", 20, "Argentina", "Buenos Aires",88);
 
 
 
-
+SELECT *
+FROM product p1
+WHERE nbachat = (
+    SELECT MAX(nbachat)
+    FROM product p2
+    WHERE p1.category = p2.category
+);
