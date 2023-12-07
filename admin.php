7f73178d1c8b6f064d2eadb09faf4b4182102f1e
imane-bouabidi <imaneyoucode@gmail.com>
2023-12-07 11:08:59 +0100

    panier + footer + sign

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 session_start(); 
     require 'back/connexion/host.php';
-    if(!isset($_SESSION['user'])){
+    if(!isset($_SESSION['admin'])){
       header('location:sign.php?error=sdlkfjsldkjf');
     }
     else{
@@ -137,22 +137,13 @@ include('header.php');
       </div>
     </div>
   </div>
-  
+  <?php 
+    include('footer.php');
+  ?>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  
   
   <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
   <script src="roleselect.js"></script>
-
-
-
-
-
-
-
-
-
-
 
 </body>
 
